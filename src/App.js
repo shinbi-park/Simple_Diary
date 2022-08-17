60479f8a7d68f7b27bf93ee3a6b8d67170582565
Pingu <tlsql665@naver.com>
2022-08-17 22:40:20 +0900

    일기 리스트 랜더링(dummyList 사용)
    dummyList 배열을 prop으로 전달 받아 map을 이용하여 화면에 리스트 형태로 랜더링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "박신비",
+    content: "냥냥 1",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "김민성",
+    content: "냥냥 2",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "르블랑",
+    content: "냥냥 3",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
---
+++ src/DiaryItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const DiaryItem = ({ author, content, created_date, id, emotion }) => {
+  return (
+    <div className="DiaryItem">
+      <div className="info">
+        <span>
+          작성자 : {author} | 감정점수 : {emotion}{" "}
+        </span>
+        <span className="date">{new Date(created_date).toLocaleString()}</span>
+      </div>
+      <div className="content">{content}</div>
+    </div>
+  );
+};
+
+export default DiaryItem;
