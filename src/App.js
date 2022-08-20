0debc95e803c9db1277da09f9a8fbc90e3728730
Pingu <tlsql665@naver.com>
2022-08-20 20:44:04 +0900

    api를 호출하여 일기데이터의 초기값 설정(jsonplaceholder 이용)
    useEffect 를 이용하여 mount 시 초기값 작성하는 함수 실행
    async await, fetch를 이용하여 api호출하고, 해당 데이터를 적절하게 가공 및 slice를 이용하여 20개의 일기 초기 리스트를 작성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+
+//https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_data: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
@@ -23,7 +48,7 @@ function App() {
   const onRemove = (targetId) => {
     setData(data.filter((it) => it.id !== targetId));
   };
-  // App컴포넌트에서 직접 onDelete를 호출하는 것이 아니기 때문에 매개변수 targetId를 전달받는다.
+  // App컴포넌트에서 직접 onRemove를 호출하는 것이 아니기 때문에 매개변수 targetId를 전달받는다.
 
   const onEdit = (targetId, newContent) => {
     setData(
